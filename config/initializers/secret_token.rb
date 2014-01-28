dd4c3e7e4ad0ab0fc5fbd2cf954c7793244898a0
Naly Rama <nalytafikarilala@gmail.com>
2014-01-28 14:51:52 +0300

    Car project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarProject::Application.config.secret_token = '25dc5bab809ba49552ef476a5b0996fa0f939c663d6ee903f302f76a61e576bd530ae4753fc7c9515255029938d25a5071c578b88653cdbcf40a20ca50b337d9'
